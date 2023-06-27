5d0732c80ceb71d76d780d25d961bd55d5faeb84
alex <alexgrimes.se@gmail.com>
2023-06-27 19:23:32 -0400

    set up

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,4 @@
+use crate::pages::home::*;
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
---
+++ src/pages/home.rs
@@ -0,0 +1,8 @@
+use leptos::*;
+
+#[component]
+fn Home(cx: Scope) -> impl IntoView {
+    view! { cx,
+        <div></div>
+    }
+}
